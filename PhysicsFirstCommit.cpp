7ca7d5df9087968011bbea02cef3365712435f8a
Pavel Yordanov <PTYordanov20@codingburgas.bg>
2021-11-27 12:42:51 +0200

    Added formulas.

--- PhysicsFirstCommit.cpp
+++ PhysicsFirstCommit.cpp
@@ -7,15 +7,16 @@ using namespace std;
 int main()
 {
 
-    cout << "Welcome to our game" << endl;
-    cout << "Please select a planet" << endl;
+    cout << "Welcome to our calculator" << endl;
+    cout << "Please select a planet:" << endl;
+
+    double mass;
+    double acceleration;
+    double time;
+    double v;
 
 
     int selector;
-    double mass;
-    double acceleration;
-    int time;
-
     int Mercury = 1;
     int Venus = 2;
     int Earth = 3;
@@ -24,65 +25,132 @@ int main()
     int Saturn = 6;
     int Uranus = 7;
     int Neptune = 8;
-    int Pluto = 9;
 
     cin >> selector;
 
-    if (selector == Mercury)
+    if (selector <= 8)
     {
-        cout << "You chose to work on Mercury" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
+        if (selector == Mercury && selector <= 8)
+        {
+            cout << "You chose to work on Mercury" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+            
 
-    }
-    if (selector == Venus)
-    {
-        cout << "You chose to work on Venus" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Earth)
-    {
-        cout << "You chose to work on Earth" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
+            double g = 3.7;
 
-    }
-    if (selector == Mars)
-    {
-        cout << "You chose to work on Mars" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Jupiter)
-    {
-        cout << "You chose to work on Jupiter" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Saturn)
-    {
-        cout << "You chose to work on Saturn" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Uranus)
-    {
-        cout << "You chose to work on Uranus" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Neptune)
-    {
-        cout << "You chose to work on Neptune" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
-    }
-    if (selector == Pluto)
-    {
-        cout << "You chose to work on Pluto" << endl;
-        cout << "Now please insert the mass of your object:" << endl;
-        cin >> mass;
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Venus && selector <= 8)
+        {
+            cout << "You chose to work on Venus" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+           
+
+            double g = 8.8;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Earth && selector <= 8)
+        {
+            cout << "You chose to work on Earth" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+
+            double g = 9.8;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+
+        }
+        if (selector == Mars && selector <= 8)
+        {
+            cout << "You chose to work on Mars" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+
+            double g = 3.8;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Jupiter && selector <= 8)
+        {
+            cout << "You chose to work on Jupiter" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+            
+
+            double g = 24.9;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Saturn && selector <= 8)
+        {
+            cout << "You chose to work on Saturn" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+
+            double g = 9;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Uranus && selector <= 8)
+        {
+            cout << "You chose to work on Uranus" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+
+            double g = 8.7;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
+        if (selector == Neptune && selector <= 8)
+        {
+            cout << "You chose to work on Neptune" << endl;
+            cout << "Now please insert the mass of your object and the time of travelling:" << endl;
+
+            double g = 11.2;
+
+            cin >> mass;
+            cin >> time;
+
+            v = g * time;
+
+            cout << "The speed of your object is " << v;
+        }
     }
 
+    else
+    {
+        cout << "try again";
+    }
+    return 0;
 }
