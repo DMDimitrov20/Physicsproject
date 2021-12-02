08c1f88cb9fc40fbfc864db1b869377ed9d2f610
Kaloyan Filipov <86263379+KMFilipov@users.noreply.github.com>
2021-12-02 22:00:13 +0200

    Update tetris.cpp

--- tetris.cpp
+++ tetris.cpp
@@ -1,76 +1,105 @@
-
 #include <iostream>
 
 using namespace std;
+#ifndef UNICODE
+#define UNICODE
+#endif
+#ifndef _UNICODE
+#define _UNICODE
+#endif
 
-wstring tetr[7];
+#include <Windows.h>
 
-int fieldw = 12;
-int fieldh = 18;
-unsigned char*pfield = nullptr;
+wstring tetromino[7];
+int nFieldWidth = 12;
+int nFieldHeight = 18;
+unsigned char* pField = nullptr;
+
+int nScreenWidth = 120; //y na tetris
+int nScreenHeight = 30; //x na tetris
 
 
-int rotation(int x, int y, int r)
+int Rotate(int px, int py, int r)
 {
     switch (r % 4)
     {
-    case 0: return y * 4 + x;         //0 degrees
-    case 1: return 12 + y - (x * 4);  //90 degrees
-    case 2: return 15 - (y * 4) - x;  //180 degrees
-    case 3: return 3 - y + (x * 4);   //360 degrees
+    case 0: return py * 4 + px;
+    case 1: return 12 + py - (px * 4);
+    case 2: return 15 - (py * 4) - px;
+    case 3: return 3 - py + (px * 4);
     }
     return 0;
 }
 
+
 int main()
 {
-    tetr[0].append(L"..X.");
-    tetr[0].append(L"..X.");
-    tetr[0].append(L"..X.");
-    tetr[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
 
 
-    tetr[1].append(L"..X.");
-    tetr[1].append(L".XX.");
-    tetr[1].append(L".X..");
-    tetr[1].append(L"....");
-
-
-    tetr[2].append(L"..X.");
-    tetr[2].append(L".XX.");
-    tetr[2].append(L"..X.");
-    tetr[2].append(L"....");
-
-
-    tetr[3].append(L"....");
-    tetr[3].append(L".XX.");
-    tetr[3].append(L".XX.");
-    tetr[3].append(L"....");
-
-
-    tetr[4].append(L"..X.");
-    tetr[4].append(L".XX.");
-    tetr[4].append(L"..X.");
-    tetr[4].append(L"....");
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
 
 
 
-    tetr[5].append(L"....");
-    tetr[5].append(L".XX.");
-    tetr[5].append(L"..X.");
-    tetr[5].append(L"..X.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
 
 
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
 
-    tetr[6].append(L"..X.");
-    tetr[6].append(L"..X.");
-    tetr[6].append(L".X..");
-    tetr[6].append(L".X..");
 
-    pfield = new unsigned char[fieldw * fieldh];
-  for(int l = 0; l < fieldw; l++)
-    for(int k = 0; k < fieldh; k++)
-      pfield[k * fieldw + l] = (l == 0 || l == fieldw - 1 || k == fieldh) ? 10 : 0;
-    
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"..X.");
+    tetromino[6].append(L"..X.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
+
+    pField = new unsigned char[nFieldWidth * nFieldHeight];
+
+    for (int x = 0; x < nFieldWidth; x++)
+        for (int y = 0; y < nFieldHeight; y++)
+            pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
+
+
+    wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+    for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    bool bGameOver = false;
+
+    while (!bGameOver)
+    {
+        //Draw Field
+        for (int x = 0; x < nFieldWidth; x++)
+            for (int y = 0; y < nFieldHeight; y++)
+                screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+
+        //Draw Frame
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+    }
+
     return 0;
 }
